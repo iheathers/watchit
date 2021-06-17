226d3c66581e1047f379df92a0805a0b826010db
iheathers <shresthapritam7@gmail.com>
2021-06-17 23:23:32 +0545

    Add child process to run another file from node js app

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 #!/usr/bin/env node
 
+const program = require('caporal');
 const chokidar = require('chokidar');
+const fsPromises = require('fs').promises;
+const { spawn } = require('child_process');
+const debounce = require('lodash.debounce');
 
-chokidar
-  .watch('.')
-  .on('add', (path) => console.log(`File ${path} has been added`))
-  .on('change', (path) => console.log(`File ${path} has been changed`))
-  .on('unlink', (path) => console.log(`File ${path} has been removed`));
+let proc;
+const start = debounce((file) => {
+  console.log({ file });
+  try {
+    if (proc) {
+      proc.kill();
+    }
+    console.log('>>> Starting Process');
+    proc = spawn('node', [file], { stdio: 'inherit' });
+  } catch (error) {
+    console.log(error);
+  }
+}, 100);
+
+program
+  .version('1.0.0')
+  .argument('[filename]', 'Name of file to execute')
+  .action(async ({ filename }) => {
+    const file = filename || 'index.js';
+
+    try {
+      await fsPromises.access(file);
+    } catch (error) {
+      throw new Error(`File with name: ${file} not found.`);
+    }
+
+    chokidar
+      .watch('.')
+      .on('add', () => start(file))
+      .on('change', () => start(file))
+      .on('unlink', () => start(file));
+  });
+
+program.parse(process.argv);
---
+++ test.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+  console.log('nope');
+}, 1000);
